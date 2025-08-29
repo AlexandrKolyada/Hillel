1022603ec241cffe74c156e092c8a0619ff712db
Alexandr Kolyada <alexandrkolyada@mac.speedport.ip>
2025-08-30 00:22:19 +0200

    Home work

--- Lesson_02/5.py
+++ Lesson_02/5.py
@@ -1,7 +1,3 @@
 num = int(input("Enter your number: "))
 
-print(((num % 1000) % 100) % 10)
-print((num // 10) % 10)
-print((num % 999) // 100)
-print((num // 999) % 10)
-print((num // 9999) % 10)
+print((((num % 1000) % 100) % 10),((num // 10) % 10),((num % 1000) // 100),((num // 999) % 10),((num // 9999) % 10))
