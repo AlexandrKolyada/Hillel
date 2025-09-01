9a698e8e3b44c82fe73ecc323f52acbc05f9c841
Alexandr Kolyada <alexandrkolyada@mac.speedport.ip>
2025-09-01 19:03:17 +0200

    Home work

--- Lesson_02/5.py
+++ Lesson_02/5.py
@@ -1,4 +1,9 @@
 num = int(input("Enter your number: "))
 
+a = (((num % 1000) % 100) % 10) * 10000
+b = ((num // 10) % 10) * 1000
+c = ((num % 1000) // 100) * 100
+d = ((num // 999) % 10) * 10
+x = (num // 9999) % 10
 
-print(str((((num % 1000) % 100) % 10))+str(((num // 10) % 10))+str(((num % 1000) // 100))+str(((num // 999) % 10))+str(((num // 9999) % 10)))
+print(a + b + c + d + x)
