0dd5c7bea6fba2ac2e9b2bfa45785f73199d6a06
Alexandr Kolyada <alexandrkolyada@mac.speedport.ip>
2025-09-08 18:21:47 +0200

    home_work_3.3

--- HomeWork/Split one List.py
+++ HomeWork/Split one List.py
@@ -1,13 +1,15 @@
 import math
-
+list_b = [1, 2, 3, 4, 5, 6, 7, 8, 10]
 new_list = []
-list_b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
+firs_half_list = []
+next_half_list = []
 
 if len(list_b) == 0:
-    print(new_list[:])
+    new_list.append(firs_half_list)
+    new_list.append(next_half_list)
+    print(new_list)
 else:
-    half_list = len(list_b) // 2 + 1
+    half_list = math.ceil(len(list_b) / 2)
     firs_half_list = list_b[:half_list]
     next_half_list = list_b[half_list:]
     new_list.append(firs_half_list)
