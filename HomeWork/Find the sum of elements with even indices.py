595ecdf57f28ad6e3d8879fb19123526542bf03a
Alexandr Kolyada <alexandrkolyada@mac.speedport.ip>
2025-09-09 14:40:28 +0200

    home_work_4.2

--- HomeWork/Find the sum of elements with even indices.py
+++ HomeWork/Find the sum of elements with even indices.py
@@ -1,15 +1,13 @@
-my_list = [2, 2, 2]
+my_list = [0, 1, 7, 2,  4, 8]
 i = 0
 len_list = len(my_list)
-last_element = my_list[-1]
-even = my_list[::2]
 result_list = []
 
 while i < len_list:
     if len(my_list) == 0:
         print(0)
     even_list = sum(my_list[::2])
-    result_list = even_list * last_element
+    result_list = even_list * my_list[-1]
     len_list -=1
 else:
     i += 1
