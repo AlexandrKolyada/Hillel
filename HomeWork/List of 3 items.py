75cc341ebdf44de7858f67ef3a64f6ab01f4c4e2
Alexandr Kolyada <alexandrkolyada@mac.speedport.ip>
2025-09-10 10:14:11 +0200

    home_work_4.3

---
+++ HomeWork/List of 3 items.py
@@ -0,0 +1,4 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 9]
+new_list = [my_list[0], my_list[2], my_list[-2]]
+
+print(new_list)
---
+++ Lesson_06/01.py
@@ -0,0 +1,6 @@
+my_list = [1, 4, 7.5, -9, '6rghf', 0, 3, 5, 6, 'rerter', 8, 2, 11, 15, -4, 0.5]
+
+list_int = []
+list_float = []
+list_negative = []
+
