fbfa979c2cff3670eaaff84d58247df1803f8420
Alexandr Kolyada <alexandrkolyada@mac.speedport.ip>
2025-09-10 12:38:43 +0200

    home_work_4.3

--- HomeWork/List of 3 items.py
+++ HomeWork/List of 3 items.py
@@ -1,4 +1,17 @@
-my_list = [1, 2, 3, 4, 5, 6, 7, 9]
-new_list = [my_list[0], my_list[2], my_list[-2]]
+import random
 
-print(new_list)
+full_list = list(range(1, 101))
+min_len = 3
+max_len = 10
+max_start_index = len(full_list) - min_len
+
+if max_start_index < 0:
+    print('FALSE')
+else:
+    start_index = random.randint(0, max_start_index)
+    random_len = random.randint(min_len, max_len)
+    end_index = min(start_index + random_len, len(full_list))
+    random_slice = full_list[start_index:end_index]
+    new_list2 = [random_slice[0], random_slice[2], random_slice[-2]]
+    print(random_slice)
+    print(new_list2)
---
+++ HomeWork/Variable name.py
@@ -0,0 +1,13 @@
+import keyword
+
+
+# my_variable = input('Enter your variable: ')
+all_words = keyword.kwlist
+all_words2 = keyword
+
+i = 'if'
+
+if i in all_words:
+    print("FALSE")
+else:
+    print('TRUE')
