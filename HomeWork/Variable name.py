22efc7cc6bd328b61955c9fe815e4196dbef1dc2
Alexandr Kolyada <alexandrkolyada@mac.speedport.ip>
2025-09-11 19:30:08 +0200

    home_work_5.1

--- HomeWork/Variable name.py
+++ HomeWork/Variable name.py
@@ -3,11 +3,16 @@ import keyword
 
 my_variable = str(input('Enter your variable: '))
 all_words = keyword.kwlist
-all_words2 = keyword
 
-i = 'if'
-
-if i in all_words:
+if my_variable in all_words:
+    print("FALSE")
+elif any(symbol.isupper() for symbol in my_variable):
+    print("FALSE")
+elif any(symbol in r"!\"#$%&'()*+,-./:;<=>?@[\]^`{|}~" for symbol in my_variable):
+    print("False")
+elif my_variable.count("_") > 1:
+    print("FALSE")
+elif my_variable[0].isdigit():
     print("FALSE")
 else:
     print('TRUE')
