e0a2b46d8b63ea6a95a09c383f2f584e8d94d52a
Alexandr Kolyada <alexandrkolyada@mac.speedport.ip>
2025-09-12 15:20:15 +0200

    home_work_5.3

--- HomeWork/hashtag.py
+++ HomeWork/hashtag.py
@@ -1,5 +1,5 @@
 import string
 
 my_string = input("Enter you text: ")
-modify_string = my_string[:140].title().translate(str.maketrans("", "", string.punctuation)).replace(" ", "")
+modify_string = my_string.title().translate(str.maketrans("", "", string.punctuation)).replace(" ", "")[:140]
 print(f"#{modify_string}")
