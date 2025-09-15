ca0ac87f0efb53c5adcbf880ceb6c2e91f763dde
Alexandr Kolyada <alexandrkolyada@mac.speedport.ip>
2025-09-15 08:50:17 +0200

    home_work_6.1

---
+++ HomeWork/Letter range.py
@@ -0,0 +1,13 @@
+import string
+
+letter_range = input(str("Enter you letter range: "))
+letters = string.ascii_letters
+first_letter, end_letter = letter_range.split("-")
+start_index = letters.index(first_letter)
+end_index = letters.index(end_letter)
+
+for char in letters[start_index:end_index+1]:
+    if char == 0:
+        print(char)
+    else:
+        print(char, end="")
