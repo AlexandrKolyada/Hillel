5eb0ffe1fc88463ad23d20375d066796d2d685cd
Alexandr Kolyada <alexandrkolyada@mac.speedport.ip>
2025-09-16 22:31:39 +0200

    home_work_6.2

--- HomeWork/Number to date converter.py
+++ HomeWork/Number to date converter.py
@@ -1,7 +1,16 @@
+day_name1 = 'день'
+day_name2 = 'дні'
+day_name3 = 'днів'
 
 sec = int(input("Enter Your date: "))
 minutes, seconds = divmod(sec, 60)
 hours, minutes = divmod(minutes, 60)
 day, hours = divmod(hours, 24)
-print(sec, f"-> {day} днів, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
+
+if day % 10 == 1 and day % 100 != 1:
+    print(sec, f"-> {day},", day_name1 ,f"{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
+elif (str(day).endswith("2") or str(day).endswith("3") or str(day).endswith("4")) and day not in (12, 13, 14):
+    print(sec, f"-> {day},", day_name2, f"{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
+else:
+    print(sec, f"-> {day}", day_name3, f"{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
 
