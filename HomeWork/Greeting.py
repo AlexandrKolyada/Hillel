d988264762364e4d370fcf667257bea65ca27833
Alexandr Kolyada <alexandrkolyada@mac.speedport.ip>
2025-09-17 20:57:09 +0200

    home_work_7.1

--- HomeWork/Greeting.py
+++ HomeWork/Greeting.py
@@ -1,4 +1,4 @@
 def say_hi(name, age):
-    print(f"Hi. My name is {name} and I'm {age} years old")
-
-say_hi("Alex", 32)
+    name = 'Alex'
+    age = 32
+    return str(f"Hi. My name is {name} and I'm {age} years old")
