766d3ab34b3ba6dcee81e3a46bd31294fc2dd3ed
Alexandr Kolyada <alexandrkolyada@mac.speedport.ip>
2025-09-17 22:08:32 +0200

    home_work_6.3

--- HomeWork/Product of numbers.py
+++ HomeWork/Product of numbers.py
@@ -1,15 +1,11 @@
-from unicodedata import digit
-
 your_number = input("Enter your number: ")
-
 num = list(your_number)
 gen = [int(digit) for digit in your_number]
-res = 1
-
-for i in gen:
-    res *= i
-print(res)
-
-for i in res:
-    res *= i
+while True:
+    res = 1
+    for i in gen:
+        res *= i
+        gen = [int(d) for d in str(res)]
+    if res <= 9:
+        break
 print(res)
