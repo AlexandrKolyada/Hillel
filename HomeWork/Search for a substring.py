5029f9964e0ce93793a50a34f767d30e19691963
Alexandr Kolyada <alexandrkolyada@mac.speedport.ip>
2025-09-19 10:19:51 +0200

    home_work_7.3

--- HomeWork/Search for a substring.py
+++ HomeWork/Search for a substring.py
@@ -3,6 +3,10 @@ def second_index(text, letter):
     index1 = text.find(letter)
     if index1 == -1:
         return None
-    else:
-        index2 = text.find(letter, index1 + 1)
+
+    index2 = text.find(letter, index1 + 1)
+    if index2 == -1:
+        return None
     return index2
+
+print(second_index("hi", "h"))
