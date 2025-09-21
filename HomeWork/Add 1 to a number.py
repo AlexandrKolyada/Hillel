d92f7dc0f62446987ffffd1ecaf6f8afd180a9d3
Alexandr Kolyada <alexandrkolyada@mac.speedport.ip>
2025-09-21 23:06:25 +0200

    home_work_8.1

--- HomeWork/Add 1 to a number.py
+++ HomeWork/Add 1 to a number.py
@@ -1,10 +1,10 @@
-def add_one(gen_list):
+def add_one(some_list):
 
-    number = [9, 9, 9, 9]
+    number = [9]
     gen = [str(i) for  i in number]
     string_number = "".join(gen)
     new_list = int(string_number) + 1
-    gen_list = [int(i) for i in str(new_list)]
-    return (gen_list)
+    some_list = [int(i) for i in str(new_list)]
+    return (some_list)
 
-print(add_one(gen_list=""))
+print(add_one(some_list=""))
