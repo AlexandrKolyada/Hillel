09bd238a8cb268aec801af427a45b5a6f7967411
Alexandr Kolyada <alexandrkolyada@mac.speedport.ip>
2025-09-22 22:33:21 +0200

    home_work_8.3

--- HomeWork/Add 1 to a number.py
+++ HomeWork/Add 1 to a number.py
@@ -3,8 +3,8 @@ def add_one(some_list):
 
     gen = [str(i) for  i in some_list]
     string_number = "".join(gen)
-    new_list = int(string_number) + 1
-    some_list = [int(i) for i in str(new_list)]
-    return (some_list)
+    input_list = int(string_number) + 1
+    output_list = [int(i) for i in str(input_list)]
+    return (output_list)
 
-print(add_one(some_list=[9, 9, 9]))
+print(add_one(some_list=[9, 9, 9, 8]))
--- HomeWork/Unique number.py
+++ HomeWork/Unique number.py
@@ -1,2 +1,8 @@
+def find_unique_value():
+    my_list = [2, 3, 3, 3, 5, 5]
+    for unique_value in my_list:
+        if my_list.count(unique_value) == 1:
+            return (unique_value)
 
-my_list = [1, 2, 1, 1]
+
+print(find_unique_value())
