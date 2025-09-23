43e3011a618ccc83102c6c8b250062685bbefe02
Alexandr Kolyada <alexandrkolyada@mac.speedport.ip>
2025-09-23 17:48:17 +0200

    home_work_8.2

--- HomeWork/PAlindrome.py
+++ HomeWork/PAlindrome.py
@@ -1,6 +1,5 @@
 def is_palindrome(some_text):
-    some_text = "madam"
-    some_text.islower()
+    some_text.lower()
     return some_text == some_text[:: -1]
 
-print(is_palindrome(some_text=""))
+print(is_palindrome(some_text="A man, a plan, a canal: Panama"))
