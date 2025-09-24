49ca031ad4a7e137e3504845d38332746fef08fa
Alexandr Kolyada <alexandrkolyada@mac.speedport.ip>
2025-09-24 20:46:16 +0200

    home_work_8.2

--- HomeWork/PAlindrome.py
+++ HomeWork/PAlindrome.py
@@ -1,5 +1,11 @@
 def is_palindrome(some_text):
-    some_text.lower()
-    return some_text == some_text[:: -1]
+    import string
+    cleaned_text = some_text.lower().translate(str.maketrans("", "", string.punctuation)).replace(" ", "")
+    return cleaned_text == cleaned_text[:: -1]
 
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+print("ОК")
 print(is_palindrome(some_text="A man, a plan, a canal: Panama"))
