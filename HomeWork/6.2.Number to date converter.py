cfecf6191a51e581f0a36ea74ec74f5217c79e4f
Alexandr Kolyada <alexandrkolyada@mac.speedport.ip>
2025-09-26 23:05:08 +0200

    home_work_6.2

--- HomeWork/6.2.Number to date converter.py
+++ HomeWork/6.2.Number to date converter.py
@@ -7,10 +7,9 @@ minutes, seconds = divmod(sec, 60)
 hours, minutes = divmod(minutes, 60)
 day, hours = divmod(hours, 24)
 
-if day % 10 == 1 and day % 100 != 1:
+if day % 10 == 1 and day % 100 != 11:
     print(sec, f"-> {day},", day_name1 ,f"{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
-elif (str(day).endswith("2") or str(day).endswith("3") or str(day).endswith("4")) and day not in (12, 13, 14):
+elif day % 10 in (2, 3, 4) and day % 100 not in (12, 13, 14):
     print(sec, f"-> {day},", day_name2, f"{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
 else:
     print(sec, f"-> {day}", day_name3, f"{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
-
